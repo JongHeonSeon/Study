a19043de48d4089d2be745ee5b24920ac9da9ab2
선종헌 <acer1052@naver.com>
2022-09-23 10:35:32 +0900

    SVG Study

--- svg/js/script.js
+++ svg/js/script.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".star").on("click", function () {
-        $(this).children(".star__star").children(".star__star_star").toggleClass("hidden");
+        $(this).children(".star__star").children(".star__star_star, .star__star_star_shadow").toggleClass("hidden");
         $(this).toggleClass("active");
     });
 });
